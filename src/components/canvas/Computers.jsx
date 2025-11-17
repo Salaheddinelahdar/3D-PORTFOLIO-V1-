a0ef8940b740bede6a5362ca603c525f690455e8
salaheddinelahdar <salaheddinelahdar@gmail.com>
2025-11-17 22:40:30 +0100

    fix: disable shadows on computer model to prevent Chrome crash

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,25 +1,24 @@
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState, useMemo } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("/desktop_pc/scene.gltf"); // Absolute path
 
   return (
     <mesh>
-      {/* UPDATE 1: Increased intensity from 0.15 to 4 */}
       <hemisphereLight intensity={4} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
-        castShadow
+        // --- THIS LINE WAS REMOVED ---
+        // castShadow 
         shadow-mapSize={1024}
       />
-      {/* UPDATE 2: Increased intensity from 1 to 2 */}
       <pointLight intensity={2} />
       <primitive
         object={computer.scene}
@@ -35,21 +34,12 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
-
-    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
-
-    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
@@ -58,7 +48,7 @@ const ComputersCanvas = () => {
   return (
     <Canvas
       frameloop='demand'
-      shadows
+      shadows={!isMobile} // Shadows disabled on mobile
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
